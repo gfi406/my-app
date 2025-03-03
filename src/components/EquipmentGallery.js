379eb723d043475511159593bc0ad466661c278a
gfi406 <makafanasiev@gmail.com>
2025-03-03 19:24:00 +0300

    upd again

--- src/components/EquipmentGallery.js
+++ src/components/EquipmentGallery.js
@@ -8,7 +8,7 @@ const EquipmentGallery = () => {
   const equipmentList = [
     { 
       id: 1, 
-      title: 'Корпоративы', 
+      title: 'Комерция', 
       image: [
         'https://party2go.ru/wp-content/uploads/2022/04/25.jpg',
         'https://aksioma.rent/assets/img/totemy/d6929886cfff118b8902a1a8c0b5dbaf.jpg',
@@ -22,13 +22,43 @@ const EquipmentGallery = () => {
     },
     { 
       id: 2, 
-      title: 'Свадьбы', 
+      title: 'Фестивали', 
+      image: [
+        'https://s0.rbk.ru/v6_top_pics/media/img/8/21/755522302148218.jpeg', 
+        'https://aksioma.rent/assets/img/totemy/d6929886cfff118b8902a1a8c0b5dbaf.jpg',
+        'https://news.store.rambler.ru/img/a672e36897d359c94369a36ec38734db?img-format=auto&img-1-resize=height:400,fit:max&img-2-filter=sharpen'
+      ],
+      description: '',
+      examples: [
+        { image: 'https://aksioma.rent/assets/img/totemy/d6929886cfff118b8902a1a8c0b5dbaf.jpg', description: 'Световое оборудование 1-8 тотемов', price: '10,000 - 30,000 рублей',title: 'Световое оборудование' },
+        { image: 'https://pop-music.ru/upload/medialibrary/68e/68ef33b0c36b868224aa65e5145b6d66.jpg', description: 'Звуковое оборудование 2-6кВТ', price: '10,000 - 30,000 рублей',title: 'Звуковое оборудование' },
+      ]
+      
+    },
+    { 
+      id: 3, 
+      title: 'Свадьбы, юбилеи, поминки', 
       image: [
         'https://kasla-wed.ru/upload/iblock/af4/6mp05ltpsiaww1bxt92yiolgvm1s277x.jpg', 
         'https://aksioma.rent/assets/img/totemy/d6929886cfff118b8902a1a8c0b5dbaf.jpg',
         'https://news.store.rambler.ru/img/a672e36897d359c94369a36ec38734db?img-format=auto&img-1-resize=height:400,fit:max&img-2-filter=sharpen'
       ],
-      description: 'Световая и звуковая техника для организации незабываемых свадебных торжеств.',
+      description: '',
+      examples: [
+        { image: 'https://aksioma.rent/assets/img/totemy/d6929886cfff118b8902a1a8c0b5dbaf.jpg', description: 'Световое оборудование 1-8 тотемов', price: '10,000 - 30,000 рублей',title: 'Световое оборудование' },
+        { image: 'https://pop-music.ru/upload/medialibrary/68e/68ef33b0c36b868224aa65e5145b6d66.jpg', description: 'Звуковое оборудование 2-6кВТ', price: '10,000 - 30,000 рублей',title: 'Звуковое оборудование' },
+      ]
+      
+    },
+    { 
+      id: 4, 
+      title: 'Обслуживание', 
+      image: [
+        'https://i.pinimg.com/736x/b6/5f/0d/b65f0d91d2289c434b8d3eab4c300823.jpg', 
+        'https://media.istockphoto.com/id/641494268/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA-%D1%81-%D0%BC%D0%BE%D0%BB%D0%BE%D1%82%D0%BA%D0%BE%D0%BC-%D0%B2-%D1%80%D1%83%D0%BA%D0%B5.jpg?s=612x612&w=0&k=20&c=CfATyfnEv9dKQt7uMuC1z1oAIpmQ7AZdJDdC1n7oRhw=',
+        'https://news.store.rambler.ru/img/a672e36897d359c94369a36ec38734db?img-format=auto&img-1-resize=height:400,fit:max&img-2-filter=sharpen'
+      ],
+      description: '',
       examples: [
         { image: 'https://aksioma.rent/assets/img/totemy/d6929886cfff118b8902a1a8c0b5dbaf.jpg', description: 'Световое оборудование 1-8 тотемов', price: '10,000 - 30,000 рублей',title: 'Световое оборудование' },
         { image: 'https://pop-music.ru/upload/medialibrary/68e/68ef33b0c36b868224aa65e5145b6d66.jpg', description: 'Звуковое оборудование 2-6кВТ', price: '10,000 - 30,000 рублей',title: 'Звуковое оборудование' },
@@ -165,7 +195,7 @@ const EquipmentCard = ({ item, navigate }) => {
       {/* Контент */}
       <div className="p-4">
         <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
-        <p className="text-gray-600 mb-2">{item.description}</p>
+        {/* <p className="text-gray-600 mb-2">{item.description}</p> */}
         {/* <button className="text-blue-600 font-medium hover:text-blue-800">Подробнее →</button> */}
       </div>
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,13 @@ import React from "react";
 const Hero = () => {
   return (
     <section 
-      className="pt-32 pb-20 px-4 bg-cover bg-center" 
-      style={{ backgroundImage: 'url(https://34mag.net/piarshak/content/editor_images/2018content/september/15sovetov_koncert/125.jpg)' }}
+      className="pt-32 pb-20 px-4 bg-cover bg-center flex items-center justify-center" 
+      style={{ backgroundImage: 'url(https://34mag.net/piarshak/content/editor_images/2018content/september/15sovetov_koncert/125.jpg)', height: '80vh' }}
     >
       <div className="container mx-auto text-center">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-6 break-words">
-          Техническое оснащение мероприятий
+          Профессиональное оборудование в аренду
         </h1>
-        {/* <p className="text-lg md:text-xl text-white mb-8 max-w-2xl mx-auto">
-          Предоставляем качественное оборудование для вашего бизнеса и проектов
-        </p> */}
       </div>
     </section>
   );
